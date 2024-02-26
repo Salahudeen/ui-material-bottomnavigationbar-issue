392580f922f9d35d3ee96cfaf887cf0eb2826dd0
Salahudeen Sainulabdeen <salahudeen@salahudeens-mbp.speedport.ip>
2024-02-26 17:42:41 +0100

    issue: first router-outlet loads, then all tabs go blank

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core'
 import { Routes } from '@angular/router'
-import { NativeScriptRouterModule } from '@nativescript/angular'
+import { NativeScriptRouterModule, NSEmptyOutletComponent } from '@nativescript/angular'
 
-const routes: Routes = [ 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/(homeTab:home/default//browseTab:browse/default//searchTab:search/default)',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: NSEmptyOutletComponent,
+    loadChildren: () => import('~/app/modules/home/home.module').then((m) => m.HomeModule),
+    outlet: 'homeTab',
+  },
+  {
+    path: 'browse',
+    component: NSEmptyOutletComponent,
+    loadChildren: () => import('~/app/modules/browse/browse.module').then((m) => m.BrowseModule),
+    outlet: 'browseTab',
+  },
+  {
+    path: 'search',
+    component: NSEmptyOutletComponent,
+    loadChildren: () => import('~/app/modules/search/search.module').then((m) => m.SearchModule),
+    outlet: 'searchTab',
+  },
 ]
 
 @NgModule({
